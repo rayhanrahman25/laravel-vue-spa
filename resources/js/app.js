5e7f75045c288ee7f9046cb3195ae9f37dcfaef1
rayhanrahman25 <rayhanrahman880@gmail.com>
2023-09-26 20:30:41 +0600

    added: vue, vue-route, tailwind

---
+++ resources/js/app.js
@@ -0,0 +1,8 @@
+import '../css/app.css';
+import {createApp} from 'vue'
+import router from "./router/index.js"
+import App from './App.vue'
+
+const app = createApp(App)
+app.use(router)
+app.mount("#app")
---
+++ resources/js/views/Home.vue
@@ -0,0 +1,6 @@
+<template>
+    <Heading/>
+</template>
+<script setup>
+ import Heading from '../components/Heading.vue';
+</script>
